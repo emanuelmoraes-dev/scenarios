881223ef2f6b42feb730958c1461318923b3a232
Emanuel Moraes <emanuelmoraes297@gmail.com>
2021-08-20 21:47:12 -0300

    setup snowpack

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,68 @@
 <script lang="ts">
-	export let name: string
+    import {onMount} from 'svelte'
+    let count: number = 0
+    onMount(() => {
+        const interval = setInterval(() => count++, 1000)
+        return () => {
+            clearInterval(interval)
+        }
+    })
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style lang="less">
+    :global(body) {
+        margin: 0;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .App {
+        text-align: center;
+    }
+    .App code {
+        background: #0002;
+        padding: 4px 8px;
+        border-radius: 4px;
+    }
+    .App p {
+        margin: 0.4rem;
+    }
+    .App-header {
+        background-color: #f9f6f6;
+        color: #333;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: calc(10px + 2vmin);
+    }
+    .App-link {
+        color: #ff3e00;
+    }
+    .App-logo {
+        height: 36vmin;
+        pointer-events: none;
+        margin-bottom: 3rem;
+        animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+    }
+    @keyframes App-logo-spin {
+        from {
+            transform: scale(1);
+        }
+        to {
+            transform: scale(1.06);
+        }
+    }
 </style>
+
+<div class="App">
+    <header class="App-header">
+        <img src="/logo.svg" class="App-logo" alt="logo"/>
+        <p>Edit <code>src/App.svelte</code> and save to reload.</p>
+        <p>Page has been open for <code>{count}</code> seconds.</p>
+        <p>
+            <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+                Learn Svelte
+            </a>
+        </p>
+    </header>
+</div>
